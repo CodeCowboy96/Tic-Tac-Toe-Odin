7d468e7de203d998430f6f9a492ce9cf146f3883
Tyler <179739422+CodeCowboy96@users.noreply.github.com>
2025-04-23 14:37:31 -0500

    Initialize files and write pseudocode

---
+++ javascript.js
@@ -0,0 +1,21 @@
+var gameBoard = (function(){
+    //object to handle gameboard state 
+    //first store an empty 3x3 array 
+    //take in either X or O at a specified position within the array 
+    //update display to show last players choice and current game board
+
+})()
+
+
+var gameState = (function(){
+    //needs to run every time gameboard changes
+    //checks gameBoard array for 3 X's or 3 O's in a row in any direction.
+    //declares winner if above winning condition is met.
+})()
+
+
+const people (Object)
+    //needs to store up to 2 player names
+    // assign X's to player 1 and O's to player 2
+
+
